699e28c6ba47bf5e437b361f54e068a86d2b4be7
Hannah Alverson <107658659+alverson98@users.noreply.github.com>
2022-10-11 14:44:23 -0600

    created user model

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,41 @@
-// User -
-// id
-// Username - email
-// Password
-// Name
-// Array of trip ids
+const { Schema, model } = require("mongoose");
+
+//Will use this later once we add password authentication
+//const bcrypt = require("bcrypt");
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match: [
+      /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/,
+      "Must be a valid email address.",
+    ],
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  trips: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Trip",
+    },
+  ],
+});
+
+const User = model("User", userSchema);
+
+module.exports = User;
