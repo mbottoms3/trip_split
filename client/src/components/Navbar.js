fbf7249bc5902a027e873ebdfe64283fbe565fac
Pyrotchnst <csundheim@gmail.com>
2022-10-12 14:48:28 -0600

    nav and gitignore

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,64 +2,54 @@ import React from "react";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg navbar fixed-top navbar-light bg-light">
-      <a class="navbar-brand" href="#">
-        The places we shall go!
-      </a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul class="navbar-nav">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">
-              Home <span class="sr-only">(current)</span>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Features
-            </a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">
-              Pricing
-            </a>
-          </li>
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdownMenuLink"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown link
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <a class="dropdown-item" href="#">
-                Action
+    <div>
+      <h1></h1>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">
+          The places we shall go!
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="#">
+                Home <span class="sr-only">(current)</span>
               </a>
-              <a class="dropdown-item" href="#">
-                Another action
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                My Trips
               </a>
-              <a class="dropdown-item" href="#">
-                Something else here
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Add/Join Trip
               </a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Login
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                Logout
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 }
 export default Navbar;
