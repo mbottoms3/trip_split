927f0872418b586d51f43cab3778d294afebf644
Jordan <jordan.laczkowski23@gmail.com>
2022-10-12 11:08:11 -0600

    added import to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer />
     </div>
   );
 }
