6776d4dfd862c5b51c974c77176059c21f4ac90d
Hannah Alverson <107658659+alverson98@users.noreply.github.com>
2022-10-12 12:22:38 -0600

    prevent id from being created for schemas

--- server/models/Payment.js
+++ server/models/Payment.js
@@ -2,6 +2,8 @@ const { Schema } = require("mongoose");
 
 //will be referenced as an array of objects in trip model
 const totalPaidSchema = new Schema({
+  //preventing id being created
+  _id: { id: false },
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
@@ -13,6 +15,8 @@ const totalPaidSchema = new Schema({
 
 //will be referenced as an array od objects in trip model
 const expensePaidSchema = new Schema({
+  //preventing id being created
+  _id: { id: false },
   user: {
     type: Schema.Types.ObjectId,
     ref: "User",
