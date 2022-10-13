07a182387853c6e1a3842e45f6541ac6bece2654
Hannah Alverson <107658659+alverson98@users.noreply.github.com>
2022-10-13 10:34:03 -0600

    added user id's to payment objects

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -13,43 +13,27 @@ db.once("open", async () => {
   const trips = await Trip.insertMany(tripData);
   const users = await User.insertMany(userData);
 
-  // const Tobj = tripData.totalPaid[0];
-  // Tobj.assign(Tobj, userData[0].firstName);
-  // console.log(Tobj);
-  let tempTripGlobal;
-
   for (newUser of users) {
+    //adding users to trip
     let tempTrip = trips[Math.floor(Math.random() * trips.length)];
     tempTrip.users.push(newUser._id);
     await tempTrip.save();
 
+    //adding trip to users
     const tempUser = users[Math.floor(Math.random() * users.length)];
     newUser.trips = tempTrip._id;
     await newUser.save();
-    // console.log(tempTrip);
 
-    // totalObj.forEach((object) => console.log(object));
-    // console.log(
-    //   totalObj.map((object) => {
-    //     return { ...object, user: newUser._id };
-    //   })
-    // );
-    // console.log(tempUser._id);
+    //assigning user Id to total paid objects --> will be in user order
+    for (i = 0; i < tempTrip.totalPaid.length; i++) {
+      tempTrip.totalPaid[i].user = users[i]._id;
+    }
 
-    const totalObj = tempTrip.totalPaid;
-    const amount0 = totalObj;
-    const item0 = Object.assign(amount0[0], { user: newUser._id });
-    await tempTrip.save();
-
-    console.log(item0);
-    tempTripGlobal = tempTrip;
+    //assigning user Id to expenses paid objects --> will be in user order
+    for (i = 0; i < tempTrip.expensesPaid.length; i++) {
+      tempTrip.expensesPaid[i].user = users[i]._id;
+    }
   }
-
-  // const totalObj = tempTripGlobal.totalPaid;
-  // const amount0 = totalObj[0];
-  // const item0 = Object.assign(amount0, { user: newUser._id });
-  // await tempTripGlobal.save();
-  // console.log(item0);
 
   console.log("all done!");
   process.exit(0);
