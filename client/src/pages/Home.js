f988c8719216ea2866d966937b628aabbed6c57c
Jordan <jordan.laczkowski23@gmail.com>
2022-10-14 16:20:01 -0600

    removed homepage text

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,6 @@ import "./Home.css";
 const Home = () => {
   return (
     <div className="homepage">
-      <h4>HOMEPAGE </h4>
       <h5> Be sure to login in to see your trips </h5>
     </div>
   );
