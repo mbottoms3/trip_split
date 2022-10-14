e69eded2d948d024cfa5945f0bf7e2fa338cf672
Jordan <jordan.laczkowski23@gmail.com>
2022-10-14 15:56:41 -0600

    message to login

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,11 @@
 import React from "react";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="homepage">
-      <h4>HOMEPAGE</h4>
+      <h4>HOMEPAGE </h4>
+      <h5> Be sure to login in to see your trips </h5>
     </div>
   );
 };
